6e938eb3368d4fa4aafa24e066b7024bd8418c80
Derek Allred <derekmallred@gmail.com>
2017-12-12 18:51:28 -0700

    let const and var

--- sandbox_js/app.js
+++ sandbox_js/app.js
@@ -9,3 +9,21 @@ console.time('test');
     console.log('doing more stuff');
     //time and timeEnd need same identifier, in this case its 'test'
 console.timeEnd('test');
+
+//let, const, var
+
+// let is like var in the sense that it can be changed, but it is block scoped, vars are function scoped
+// const is where you dont want a value to change, note that with objects, functions, and arrays you can change data
+// inside these data structures but not the type its self. 
+
+var lastName = 'Allred';
+let firstName = 'Derek';
+const gender = 'Male';
+
+const person = {
+    name: 'Derek',
+    age: 27
+}
+
+person.age += 1;  //valid
+person = 'me'; // not valid

0e5bdb3a08c9460b28ccf788fbe117e5d619c1d1
Derek Allred <derekmallred@gmail.com>
2017-12-12 21:11:44 -0700

    other fundamental info

--- sandbox_js/app.js
+++ sandbox_js/app.js
@@ -1,3 +1,6 @@
+/*
+    CONSOLE OBJECT
+*/
 console.log('Hey There');
 console.table({a: 3, b: 4, c: 6});
 console.warn('This is a warning');
@@ -10,7 +13,9 @@ console.time('test');
     //time and timeEnd need same identifier, in this case its 'test'
 console.timeEnd('test');
 
-//let, const, var
+/*
+    LET CONST VAR
+*/
 
 // let is like var in the sense that it can be changed, but it is block scoped, vars are function scoped
 // const is where you dont want a value to change, note that with objects, functions, and arrays you can change data
@@ -28,3 +33,49 @@ const person = {
 person.age += 1;  //valid
 //person = 'me'; // not valid
 console.warn(person);
+
+/*
+    PRIMITIVE TYPES AND REFERENCE TYPES
+*/
+
+const primitiveType = 'stored on the stack, stored directly in the location the variable access';
+const referenceType = 'objects that are stored on the heap, accessed by reference, a pointer to a location in memory';
+
+const primitiveDataTypes = [
+    'string',
+    number,
+    true,   // boolean
+    null,
+    undefined,
+    symbol
+]
+// examples
+const stringVal = 'String here';
+const numberVal = 23;
+const isCool = true;
+const car = null;
+let thing = undefined;
+let sym = Symbol();
+
+const referenceDataTypes = [
+    'arrays',
+    'object literals',
+    'functions',
+    'Dates',
+    'anything else'
+]
+
+/*
+    TYPE CONVERSION
+*/
+
+// Implicit Conversion
+const implicitConversion = 'This is where a variable is changed by another action taken, like adding a string number and a number number, it concatinates the two it does not sum them';
+
+// Explicit Conversion
+const explicitConversion = 'This is where you use a method like String(), or Number() and pass in the value to convert it to another type';
+
+/*
+    Math Object & Numbers
+*/
+

2b1d5cfb03439b15616990658c58e530af6b4e4b
Derek Allred <derekmallred@gmail.com>
2017-12-18 17:14:23 -0700

    small change to logging

--- sandbox_js/app.js
+++ sandbox_js/app.js
@@ -8,7 +8,7 @@ console.error('This is an ERROR');
 console.time('test');
     //do some stuff here and when it hit the timeEnd it will tell you how long it took to do the stuff here
     console.log('doing stuff');
-    console.log('doing stuff');
+    console.log('doing other stuff');
     console.log('doing more stuff');
     //time and timeEnd need same identifier, in this case its 'test'
 console.timeEnd('test');

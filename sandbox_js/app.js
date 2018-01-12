72ee3be792db8d63872a3e43dbf78973491f84a9
Derek Allred <derekmallred@gmail.com>
2018-01-12 14:30:00 -0700

    while/doWhile loops

--- sandbox_js/app.js
+++ sandbox_js/app.js
@@ -259,3 +259,14 @@ for(let i = 0; i < 10; i++) {
     }
     console.log(i);
 }
+
+let i = 0;
+while(i < 10) {
+    conoel.log(i);
+    i++;
+}
+
+do {
+    console.log(i);
+    i++;
+} while(i < 10);
